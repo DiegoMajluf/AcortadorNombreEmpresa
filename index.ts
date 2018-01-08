ed0ffcd492faae0647e80ed4e8a24e0fe80bf909
Diego Majluf <diego.majluf@gmail.com>
2018-01-08 13:00:51 -0300

    corrección reg limita n° de palabras

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-export function acortar(nom: string): string {
-    let reg = /^(soc.*|sac|saci|serv.*|imp.*|exp.*|ind.*|hijo.*|spa|limita.*|ltda|sa|eirl|inv.*|com.*|ing.*|cia|de|la|del|las|los)$/g
+export function acortar(nom: string, maxLen?: number, maxWords: number = 2): string {
+    let reg = /(soc.*|sac|saci|serv.*|imp.*|exp.*|ind.*|hijo.*|spa|limita.*|ltda|sa|eirl|inv.*|com.*|ing.*|cia|de|la|del|las|los)/
     let arr = nom.replace(/\./g, '')
         .split(' ')
         .filter(s => s.length > 1)
@@ -9,7 +9,9 @@ export function acortar(nom: string): string {
 
 
     //.map(s => s.substr(0, 5)) //máximo 5 carcateres por palabra
-    // .slice(0, 2) //a lo más tres nombres
+    // 
 
-    return arr.join(' ')
+    return arr
+        .slice(0, maxWords - 1)
+        .join(' ').substr(0, maxLen)
 }
