e103e3e9342086213361278651e79eb4e212179c
Diego Majluf <diego.majluf@gmail.com>
2020-12-15 18:28:02 -0300

    mejora regex

--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
 export function acortar(nom: string, maxLen: number = 15, maxWords: number = 3): string {
-    let reg = /(soc.*|sac|saci|serv.*|imp.*|exp.*|ind.*|hijo.*|spa|represent.*|limita.*|ltda|sa|eirl|inv.*|com.*|ing.*|cia|de|la|del|las|los)/
-    let arr = nom.replace(/\./g, '')
+    let reg = /^(soc.*|sac|saci|serv.*|impo.*|expo.*|ind.*|hijo.*|spa|represent.*|limita.*|ltda|sa|eirl|inv.*|com.*|ing.*|cia|de|la|del|las|los)$/
+    return nom.replace(/\./g, '')
         .split(' ')
         .filter(s => s.length > 1)
         .map(s => s.toLocaleLowerCase())
         .filter(s => !reg.test(s))
         .map(s => s.charAt(0).toUpperCase() + s.substr(1)) //capitalizar
-
-
-    //.map(s => s.substr(0, 5)) //máximo 5 carcateres por palabra
-    // 
-
-    return arr
         .slice(0, maxWords)
         .join(' ').substr(0, maxLen)
+
+
 }
